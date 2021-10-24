63ad1ffd4913c641a1626f1e2c0669488e631726
VirrenKJ <virendra.kjarwal.cer12@iitbhu.ac.in>
2021-10-24 20:40:41 +0530

    Company

--- src/app/company-listing/models/company.ts
+++ src/app/company-listing/models/company.ts
@@ -2,6 +2,7 @@ export class Company {
 	public id: number;
 	public name: string;
 	public dbName: string;
+	public dbUuid: string;
 	public email: string;
 	public contactNumber: number;
 	public industryType: string;
