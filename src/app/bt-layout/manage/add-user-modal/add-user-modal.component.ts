38c499b4cfd3af5e17c4509a049cf2fd270a3f88
VirrenKJ <virendra.kjarwal.cer12@iitbhu.ac.in>
2022-01-22 19:46:43 +0530

    Add User from Manage

--- src/app/bt-layout/manage/add-user-modal/add-user-modal.component.ts
+++ src/app/bt-layout/manage/add-user-modal/add-user-modal.component.ts
@@ -1,49 +1,178 @@
-import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output, TemplateRef, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Role } from 'src/app/authentication/common/models/role';
+import { User } from 'src/app/authentication/common/models/user';
+import { CustomValidationService } from 'src/app/authentication/services/custom-validation.service';
+import { UserService } from 'src/app/authentication/services/user.service';
+import { PaginationCriteria } from 'src/app/base/models/pagination_criteria';
+import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-add-user-modal',
-  templateUrl: './add-user-modal.component.html',
-  styleUrls: ['./add-user-modal.component.css'],
-  providers: [NgbModalConfig, NgbModal],
+	selector: 'app-add-user-modal',
+	templateUrl: './add-user-modal.component.html',
+	styleUrls: ['./add-user-modal.component.css'],
+	providers: [NgbModalConfig, NgbModal],
 })
 export class AddUserModalComponent implements OnInit {
-  @ViewChild('addUser') addUser: TemplateRef<any>;
+	@ViewChild('addUser') addUser: TemplateRef<any>;
+	@Output() userListEvent = new EventEmitter();
 
-  constructor(config: NgbModalConfig, private modalService: NgbModal) {
-    config.backdrop = 'static';
-    config.keyboard = false;
-  }
+	projectList = new Array<User>();
+	paginationCriteriaUser = new PaginationCriteria();
+	showPassword: boolean = false;
+	showConfirmPassword: boolean = false;
 
-  ngOnInit(): void {}
+	userForm: FormGroup;
 
-  @Input()
-  set openUserModal(data: any) {
-    if (data && !data.userId) {
-      this.openModal(this.addUser);
-    }
-    if (data && data.userId) {
-      this.openModal(this.addUser, data.userId);
-    }
-  }
+	constructor(
+		config: NgbModalConfig,
+		private _snackBar: MatSnackBar,
+		private userService: UserService,
+		private customValidationService: CustomValidationService,
+		private modalService: NgbModal
+	) {
+		config.backdrop = 'static';
+		config.keyboard = false;
+	}
 
-  openModal(template: TemplateRef<any>, userId = null) {
-    setTimeout(() => {
-      this.modalService.open(template, { size: 'lg' });
-    });
-  }
+	ngOnInit(): void {
+		this.formInIt();
+		this.getUserList();
+	}
 
-  contactForm = new FormGroup({
-    firstName: new FormControl(),
-    lastName: new FormControl(),
-    email: new FormControl(),
-    gender: new FormControl(),
-    isMarried: new FormControl(),
-    country: new FormControl(),
-  });
+	formInIt() {
+		this.userForm = new FormGroup(
+			{
+				id: new FormControl(null),
+				username: new FormControl(
+					null,
+					[Validators.required, Validators.minLength(5), this.customValidationService.noWhitespace],
+					this.customValidationService.usernameValidator.bind(this.customValidationService)
+				),
+				password: new FormControl(null, [Validators.required, this.customValidationService.patternValidator]),
+				confirmPassword: new FormControl(null, Validators.required),
+				firstName: new FormControl(null, [Validators.required, this.customValidationService.noWhitespace]),
+				lastName: new FormControl(null, [Validators.required, this.customValidationService.noWhitespace]),
+				email: new FormControl(null, [Validators.required, Validators.email]),
+				enabled: new FormControl(true),
+				roles: new FormControl(),
+			},
+			{
+				validators: this.customValidationService.MatchPassword('password', 'confirmPassword'),
+			}
+		);
+	}
 
-  onSubmit() {
-    console.log('Submitted!');
-  }
+	@Input()
+	set openUserModal(data: any) {
+		if (data && !data.userId) {
+			this.formInIt();
+			this.openModal(this.addUser);
+		}
+		if (data && data.userId) {
+			this.openModal(this.addUser, data.userId);
+		}
+	}
+
+	openModal(template: TemplateRef<any>, userId = null) {
+		if (userId) {
+			this.getUser(userId);
+		}
+		setTimeout(() => {
+			this.modalService.open(template, { size: 'lg' });
+		});
+	}
+
+	addUpdateUser() {
+		if (this.userForm.valid) {
+			//add role
+			let roles = new Array<Role>();
+			let role = new Role();
+			role.roleId = 1;
+			roles.push(role);
+			this.userForm.get('roles').patchValue(roles);
+
+			//remove whitespace
+			this.userForm.get('username').setValue(this.userForm.get('username').value.trim());
+			this.userForm.get('firstName').setValue(this.userForm.get('firstName').value.trim());
+			this.userForm.get('lastName').setValue(this.userForm.get('lastName').value.trim());
+			this.userForm.get('email').setValue(this.userForm.get('email').value.trim());
+
+			// post user
+			this.userService.add(this.userForm.value).subscribe(
+				response => {
+					console.log(response);
+					this.confirmationPopup('User Registered');
+				},
+				errorRes => {
+					console.log(errorRes);
+					this.snackBarPopup(errorRes.error.message);
+				},
+				() => {
+					this.userForm.reset();
+				}
+			);
+		} else {
+			this.snackBarPopup('Invalid Form');
+		}
+	}
+
+	getUser(userId) {
+		this.userService.getById(userId).subscribe(response => {
+			console.log(response);
+			if (response.status === 200 && response.data && response.data.user) {
+				this.userForm.setValue({
+					id: response.data.user.id,
+					name: response.data.user.name,
+					assignedId: response.data.user.assignedId,
+					deleteFlag: response.data.user.deleteFlag,
+				});
+			}
+		});
+	}
+
+	getUserList() {
+		this.userService.getList(this.paginationCriteriaUser).subscribe(
+			response => {
+				console.log(response);
+				if (response.status == 200 && response.data && response.data.project && response.data.project.list) {
+					this.projectList = response.data.project.list;
+				}
+			},
+			errorRes => {
+				console.error(errorRes);
+				this.snackBarPopup(errorRes.error.message);
+			}
+		);
+	}
+
+	password() {
+		this.showPassword = !this.showPassword;
+	}
+
+	confirmPassword() {
+		this.showConfirmPassword = !this.showConfirmPassword;
+	}
+
+	confirmationPopup(title: string) {
+		Swal.fire({
+			icon: 'success',
+			title: title,
+			showConfirmButton: false,
+			timer: 2000,
+		}).then(() => {
+			this.userListEvent.emit();
+			this.modalService.dismissAll();
+		});
+	}
+
+	snackBarPopup(message: string) {
+		this._snackBar.open(message, 'OK', {
+			duration: 3000,
+			horizontalPosition: 'center',
+			verticalPosition: 'top',
+		});
+	}
 }
